f60cb117b7f2733e2975b7277a35ea3e66903c3e
Tabix <panalien10@gmail.com>
2024-12-08 13:37:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
+
+
+    vector<string> slowa;
+ifstream plik("words.txt");
+string slowo;
+
+while (getline(plik, slowo)) {
+    slowa.push_back(slowo);
+}
+plik.close();
+
+
+
     cout << "Witaj w grze Wisielec!" << endl;
     return 0;
 }
